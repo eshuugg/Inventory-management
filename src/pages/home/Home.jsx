6bd59023ce5b2692fb7d4fe36a0e739b36129b70
Vikash Roy <vikashroy7272.com>
2022-04-08 11:19:57 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
+import "./home.css"
+import Chart from "../../components/charts/Chart"
+import {userData} from "../../dummyData";
+
+export default function Home() {
+  return (
+    <div className="home">
+    <FeaturedInfo/>
+    <Chart data={userData} title="User Analytics" grid dataKey={"Active User"}/>
+    </div>
+  )
+}
